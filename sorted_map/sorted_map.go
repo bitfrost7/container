5a92b4b69a34fdaae110938001d3e8e8ceb8765b
邹海强 <nuejiu629046@163.com>
2024-03-25 11:18:42 +0800

    sorted_map

---
+++ sorted_map/sorted_map.go
@@ -0,0 +1,47 @@
+package sorted_map
+
+import "container/list"
+
+type SortedMap[k comparable] struct {
+	l *list.List
+	m map[k]*list.Element
+}
+
+func NewSortedMap[k comparable]() *SortedMap[k] {
+	return &SortedMap[k]{
+		l: list.New(),
+		m: make(map[k]*list.Element),
+	}
+}
+
+func (m *SortedMap[k]) Insert(key k, v any) {
+	e := m.l.PushBack(v)
+	m.m[key] = e
+}
+
+func (m *SortedMap[k]) Delete(key k) {
+	e, ok := m.m[key]
+	if !ok {
+		return
+	}
+	delete(m.m, key)
+	m.l.Remove(e)
+}
+
+func (m *SortedMap[k]) Search(key k) (v any, ok bool) {
+	e, exist := m.m[key]
+	if !exist {
+		return nil, false
+	}
+	return e.Value, true
+}
+
+func (m *SortedMap[k]) Len() int {
+	return m.l.Len()
+}
+
+func (m *SortedMap[k]) Range(f func(v any)) {
+	for e := m.l.Front(); e != nil; e = e.Next() {
+		f(e.Value)
+	}
+}
